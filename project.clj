93e6c551f89b103d7f0b98cf16deae6a8bf24fec
Nicolas Buduroi <nbuduroi@gmail.com>
2010-12-21 17:41:00 -0500

    Initial commit: added the project.clj and .gitignore files.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject lobos "0.1.0-SNAPSHOT"
+  :description
+  "A library to create and manipulate SQL database schemas."
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [clj-help "0.2.0"]
+                     [clojureql "1.0.0-beta2-SNAPSHOT"]])
