65d4643265c42cbeba8f81b41df9aa1f4467944f
Nicolas Buduroi <nbuduroi@gmail.com>
2011-01-08 22:44:17 -0500

    Made ClojureQL a non-dev dependency.

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
   :description
   "A library to create and manipulate SQL database schemas."
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [clojureql "1.0.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [clj-help "0.2.0"]
-                     [clojureql "1.0.0"]
                      [com.h2database/h2 "1.2.147"]])
