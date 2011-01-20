33a3bb17ad76c27aef17e405b0af080e78d1a730
Nicolas Buduroi <nbuduroi@gmail.com>
2011-01-19 20:16:15 -0500

    Fixed confusion about snapshot version number and updated dependencies for Marginalia and cljss.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lobos "0.7.0-SNAPSHOT"
+(defproject lobos "0.6.0-SNAPSHOT"
   :description
   "A library to create and manipulate SQL database schemas."
   :dependencies [[org.clojure/clojure "1.2.0"]
@@ -6,8 +6,8 @@
                  [lobos/clojureql "1.1.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-clojars "0.6.0"]
-                     [marginalia "0.3.2"]
-                     [cljss "0.1.0"]
+                     [marginalia "0.5.0-alpha"]
+                     [cljss "0.1.1"]
                      [hiccup "0.3.1"]
                      [clj-help "0.2.0"]]
   :jar-exclusions [#"www.clj"])
