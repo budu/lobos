ef8e1e9f2fb24ac6f79d949f90b490a1ff8a4112
Nicolas Buduroi <nbuduroi@gmail.com>
2011-01-19 21:27:27 -0500

    Added the Javascript code in a separate file.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(
+    function() {
+        function rsz_if(test, elem, sz) {
+            if (test) elem.css('height', sz + 'px'); };
+        function rsz() {
+            var ws = $(window).width().toString();
+            $('body').css('font-size', ws.substring(0, ws.length - 2) + 'pt');
+            var h = $(window).height();
+            rsz_if($('#wrapper').height() < h - 10, $('#wrapper'), h - 10);
+            rsz_if($('#content').height() < h * 0.5, $('#content'), h * 0.5); };
+        SyntaxHighlighter.all();
+        window.onresize = function() { rsz(); };
+        rsz();
+    });
