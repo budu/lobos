502f9d43b167d0b040459e83ccba53f8ee263129
Nicolas Buduroi <nbuduroi@gmail.com>
2011-01-22 16:41:07 -0500

    Made the onresize window handler resize header fonts.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 $(document).ready(
     function() {
+        function rsz_font(elem) {
+            var ws = $(window).width().toString();
+            elem.css('font-size', ws.substring(0, ws.length - 2) + 'pt');
+        }
         function rsz_if(test, elem, sz) {
             if (test) elem.css('height', sz + 'px'); };
         function rsz() {
-            var ws = $(window).width().toString();
-            $('body').css('font-size', ws.substring(0, ws.length - 2) + 'pt');
+            rsz_font($('body'));
             var h = $(window).height();
             rsz_if($('#wrapper').height() < h - 10, $('#wrapper'), h - 10);
             rsz_if($('#content').height() < h * 0.5, $('#content'), h * 0.5); };
         SyntaxHighlighter.all();
+        window.onresize = function() { rsz_font($('#header')); };
         rsz();
     });
