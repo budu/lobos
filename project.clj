0eec87d9f13d585957b808ef2b787fc61d204bc2
Nicolas Buduroi <nbuduroi@gmail.com>
2011-01-23 18:43:52 -0500

    Switched back ClojureQL to the dev-dependencies and use the official snapshot release.

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description
   "A library to create and manipulate SQL database schemas."
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [lobos/clojureql "1.1.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[clojureql "1.1.0-SNAPSHOT"]
+                     [swank-clojure "1.2.1"]
                      [lein-clojars "0.6.0"]
                      [marginalia "0.5.0-alpha"]
                      [cljss "0.1.1"]
