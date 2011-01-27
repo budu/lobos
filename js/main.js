01a06be03e6dcdc1274cdf05a80de140791b0e55
Nicolas Buduroi <nbuduroi@gmail.com>
2011-01-27 10:59:44 -0500

    Improved Javascript that resize various elements.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 $(document).ready(
     function() {
+        function scrollbar() {
+            var viewportHeight = window.innerHeight
+                ? window.innerHeight
+                : $(window).height();
+            if (jQuery.browser.msie &&
+                parseInt(jQuery.browser.version) == 7)
+                    viewportHeight -= 3;
+            return viewportHeight <= $('#wrapper').height(); };
         function rsz_font(elem) {
-            var ws = $(window).width().toString();
-            elem.css('font-size', ws.substring(0, ws.length - 2) + 'pt');
-        }
-        function rsz_if(test, elem, sz) {
-            if (test) elem.css('height', sz + 'px'); };
+            var ws = ($(window).width() + (scrollbar() ? 20 : 0)).toString();
+            elem.css('font-size', ws.substring(0, ws.length - 2) + 'pt'); };
+        function rsz_if(elem, sz) {
+            if (elem.height() < sz) elem.css('height', sz + 'px'); };
         function rsz() {
-            rsz_font($('body'));
+            rsz_font($('#header > ul'));
             var h = $(window).height();
-            rsz_if($('#wrapper').height() < h - 10, $('#wrapper'), h - 10);
-            rsz_if($('#content').height() < h * 0.5, $('#content'), h * 0.5); };
+            rsz_if($('#wrapper'), h - 10);
+            rsz_if($('#content'), h * 0.5); };
         SyntaxHighlighter.all();
-        window.onresize = function() { rsz_font($('#header')); };
+        window.onresize = function() { rsz_font($('#header > ul')); };
         rsz();
     });
