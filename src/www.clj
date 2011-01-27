80d883908988d2124b456b9a4a8d28f0497f9f78
Nicolas Buduroi <nbuduroi@gmail.com>
2011-01-27 10:17:43 -0500

    Made the tracking code embedded into HTML.

--- src/www.clj
+++ src/www.clj
@@ -95,7 +95,7 @@
      (include-js "sh/js/shCore.js" "sh/js/shBrushXml.js" "sh/js/shBrushClojure.js")
      (include-css "sh/css/shCore.css" "sh/css/shThemeDjango.css")
      ;; Google Analytics Tracker
-     (include-js "js/ga-tracker.js")]
+     (javascript-tag (slurp "www/js/ga-tracker.js"))]
     [:body
      [:div#wrapper
       [:div#header
