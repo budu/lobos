3ae6e81cd615ed1be8ed59f1aad3500b895ba978
Nicolas Buduroi <nbuduroi@gmail.com>
2011-04-15 17:34:36 -0400

    Added the H2 drivers as development dependency and updated Marginalia to 0.5.0 version.

--- project.clj
+++ project.clj
@@ -5,8 +5,9 @@
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-clojars "0.6.0"]
-                     [marginalia "0.5.0-alpha"]
+                     [marginalia "0.5.0"]
                      [cljss "0.1.1"]
                      [hiccup "0.3.1"]
-                     [clj-help "0.2.0"]]
+                     [clj-help "0.2.0"]
+                     [com.h2database/h2 "1.3.154"]]
   :jar-exclusions [#"www.clj"])
