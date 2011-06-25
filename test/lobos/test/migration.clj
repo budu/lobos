e7b88513dcf0b84fe9be1ddf31cced7b790e1b2c
Nicolas Buduroi <nbuduroi@gmail.com>
2011-06-25 12:40:22 -0400

    Added file header, cleaned up namespace declaration and reformatted some code.

--- test/lobos/test/migration.clj
+++ test/lobos/test/migration.clj
@@ -1,13 +1,22 @@
+;; Copyright (c) Nicolas Buduroi. All rights reserved.
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 which can be found in the file
+;; epl-v10.html at the root of this distribution. By using this software
+;; in any fashion, you are agreeing to be bound by the terms of this
+;; license.
+;; You must not remove this notice, or any other, from this software.
+
 (ns lobos.test.migration
-  (:refer-clojure :exclude [alter compile defonce drop
-                            bigint boolean char double float time complement])
+  (:refer-clojure :exclude [complement])
   (:use clojure.test
         lobos.migration))
 
 (deftest test-complement-for-drop-table-statement
-  (is (= '(drop db (table :users (integer :id))) (complement '(create db (table :users (integer :id)))))
+  (is (= '(drop db (table :users (integer :id)))
+         (complement '(create db (table :users (integer :id)))))
       "should generate correct drop table statement"))
 
 (deftest test-complement-for-drop-schema-statement
-  (is (= '(drop db (schema :users) :cascade) (complement '(create db (schema :users))))
+  (is (= '(drop db (schema :users) :cascade)
+         (complement '(create db (schema :users))))
       "should generate correct drop schema statement with a cascade clause"))
