c2be4203decb61309b12aacff8a61269e3441037
Nicolas Buduroi <nbuduroi@gmail.com>
2011-08-18 01:09:58 -0400

    Updated project dependencies.

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
 (defproject lobos "0.8.0-SNAPSHOT"
   :description
   "A library to create and manipulate SQL database schemas."
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [lein-clojars "0.6.0"]
-                     [marginalia "0.5.0"]
+  :dev-dependencies [[swank-clojure "1.3.2"]
+                     [lein-clojars "0.7.0"]
+                     [marginalia "0.6.0"]
                      [cljss "0.1.1"]
                      [hiccup "0.3.1"]
-                     [clj-help "0.2.0"]
-                     [com.h2database/h2 "1.3.154"]]
+                     [com.h2database/h2 "1.3.158"]]
   :jar-exclusions [#"www.clj"])
