173481da0cdca95f821122356be6f73539b7ac61
Nicolas Buduroi <nbuduroi@gmail.com>
2011-11-12 18:08:26 -0500

    Replaced marginalia dependency by lein-marginalia and changed the gendoc script to generate a new documentation file for the migration namespace.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [org.clojure/java.jdbc "0.0.7"]
                  [org.clojure/tools.macro "0.1.1"]]
   :dev-dependencies [[lein-clojars "0.7.0"]
-                     [marginalia "0.6.0"]
+                     [lein-marginalia "0.6.1"]
                      [cljss "0.1.1"]
                      [hiccup "0.3.1"]
                      [com.h2database/h2 "1.3.160"]]
