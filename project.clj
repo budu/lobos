892ceaae38b699aded82c8df354e033c00ea1713
technomancy <technomancy@gmail.com>
2011-11-29 01:00:18 +0000

    Add lein-multi for testing against Clojure 1.2 as well.

--- project.clj
+++ project.clj
@@ -2,11 +2,15 @@
   :description
   "A library to create and manipulate SQL database schemas."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/java.jdbc "0.0.7"]
+                 [org.clojure/java.jdbc "0.1.1"]
                  [org.clojure/tools.macro "0.1.1"]]
   :dev-dependencies [[lein-clojars "0.7.0"]
                      [lein-marginalia "0.6.1"]
+                     [lein-multi "1.0.0"]
                      [cljss "0.1.1"]
                      [hiccup "0.3.1"]
                      [com.h2database/h2 "1.3.160"]]
+  :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]
+                      [org.clojure/java.jdbc "0.1.1"]
+                      [org.clojure/tools.macro "0.1.1"]]}
   :jar-exclusions [#"www.clj" #"config.clj" #"migrations.clj"])
