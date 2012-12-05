85924de7616b00e59125b40d8f7be736556466ea
Nicolas Buduroi <nbuduroi@gmail.com>
2012-12-04 20:59:36 -0500

    Updated project.clj file and Travis configuration to use lein2, changed default Clojure version to 1.4 and bumped version to 1.0.0-beta1.

--- project.clj
+++ project.clj
@@ -1,16 +1,21 @@
-(defproject lobos "1.0.0-SNAPSHOT"
+(defproject lobos "1.0.0-beta1"
   :description
   "A library to create and manipulate SQL database schemas."
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :url "http://budu.github.com/lobos/"
+  :license {:name "Eclipse Public License"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/java.jdbc "0.1.1"]
                  [org.clojure/tools.macro "0.1.1"]]
-  :dev-dependencies [[lein-clojars "0.7.0"]
-                     [lein-marginalia "0.6.1"]
-                     [lein-multi "1.1.0"]
-                     [cljss "0.1.1"]
-                     [hiccup "0.3.1"]
-                     [com.h2database/h2 "1.3.160"]]
-  :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]
-                      [org.clojure/java.jdbc "0.1.1"]
-                      [org.clojure/tools.macro "0.1.1"]]}
-  :jar-exclusions [#"www.clj" #"config.clj" #"migrations.clj"])
+  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             ;:1.5 {:dependencies [[org.clojure/clojure "1.5.0-beta1"]]}
+             :dev
+             {:dependencies
+              [[lein-clojars "0.7.0"]
+               [lein-marginalia "0.6.1"]
+               [lein-multi "1.1.0"]
+               [cljss "0.1.1"]
+               [hiccup "0.3.1"]
+               [com.h2database/h2 "1.3.160"]]}}
+  :aliases {"all" ["with-profile" "dev:dev,1.3"]}
+  :jar-exclusions [#"www.clj" #"config.clj" #"migrations.clj"]
+  :min-lein-version "2.0.0")
